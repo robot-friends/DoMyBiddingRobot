c39bcf2cfbe5224d0f95d49e802533bcb35ec208
KirillRodriguez <theoden8@gmail.com>
2016-02-21 11:43:07 +0000

    updated arduino code; renamed files from "sample"s to gestures etc.

--- cmp/leap_robot/leap_robot.ino
+++ cmp/leap_robot/leap_robot.ino
@@ -3,42 +3,46 @@
 const int ledPin= 13;
 
 void setup(){
-  Serial.begin(9600);
-  Robot.begin();
-  Robot.beginTFT();
-  Robot.stroke(0, 0, 0);
+	Serial.begin(9600);
+	Robot.begin();
+	Robot.beginTFT();
 }
 
 void loop() {
-  if (Serial.available())
-     command(Serial.read() - '0');  // convert the character '1'-'9' to decimal 1-9
+	if(Serial.available()) {
+		command(Serial.read() - '0');	// convert the character '1'-'9' to decimal 1-9
+		digitalWrite(ledPin, HIGH);
+	} else {
+		digitalWrite(ledPin, LOW);
+	}
 }
 
 void command(int serial) {
-  const static int INITPAUSE = 500;
-  static int pause = INITPAUSE;
-  switch(serial) {
-    case 1:
-      Robot.text("Left", 5, 5);
-      Robot.turn(10);
-      break;
-    case 2:
-      Robot.text("Right", 5, 5);
-      Robot.turn(-10);
-      break;
-    case 3:
-      Robot.text("Up", 5, 5);
-      Robot.motorsWrite(255, 255);
-      delay(pause);
-      Robot.motorsWrite(0, 0);
-      pause <<= 1;
-      break;
-    case 4:
-      Robot.text("Down", 5, 5);
-      Robot.motorsWrite(-255, -255);
-      delay(pause);
-      Robot.motorsWrite(0, 0);
-      pause = INITPAUSE;
-      break;
-  }
+	const static int INITPAUSE = 500;
+	static int pause = INITPAUSE;
+	Robot.stroke(0, 0, 0);
+	switch(serial) {
+		case 1:
+			Robot.text("Left", 5, 5);
+			Robot.turn(10);
+			break;
+		case 2:
+			Robot.text("Right", 5, 5);
+			Robot.turn(-10);
+			break;
+		case 3:
+			Robot.text("Up", 5, 5);
+			Robot.motorsWrite(255, 255);
+			delay(pause);
+			Robot.motorsWrite(0, 0);
+			pause <<= 1;
+			break;
+		case 4:
+			Robot.text("Down", 5, 5);
+			Robot.motorsWrite(-255, -255);
+			delay(pause);
+			Robot.motorsWrite(0, 0);
+			pause = INITPAUSE;
+			break;
+	}
 }
